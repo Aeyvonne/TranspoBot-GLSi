c6d88f67b4bf9dfd8155de817fda219d0d560fac
devroid26 <roidsylla@gmail.com>
2026-04-16 22:31:53 +0000

    Change default DB port from 3306 to 51768

--- transpobot/app.py
+++ transpobot/app.py
@@ -31,7 +31,7 @@ app.add_middleware(
 DB_CONFIG = {
    
     "host":     os.getenv("DB_HOST", "localhost"),
-     "port":     int(os.getenv("DB_PORT", 3306)),
+     "port":     int(os.getenv("DB_PORT", 51768)),
     "user":     os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", ""),
     "database": os.getenv("DB_NAME", "transpobot"),
